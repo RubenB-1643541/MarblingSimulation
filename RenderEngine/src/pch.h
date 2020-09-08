cc45b5f05fa119ea785e18a800c0dc8f96c39e37
ruben ballet <ruben.ballet@gmail.com>
2020-09-08 12:48:34 +0200

    Project Setup

---
+++ RenderEngine/src/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cmath>
+#include <cstring>
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <map>
+
+#ifdef PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif // PLATFORM_WINDOWS
+
---
+++ premake5.lua
@@ -0,0 +1,119 @@
+workspace "MarblingSimulation"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+	
+	startproject "Simulation"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "RenderEngine"
+	location "RenderEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "RenderEngine/src/pch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/dep/GLEW/include",
+		"%{prj.name}/dep/GLFW/include",
+		"%{prj.name}/dep/GLM/include",
+		"%{prj.name}/dep/ImGui",
+		"%{prj.name}/dep/stb_image",
+		"%{prj.name}/src"
+	}
+
+	libdirs 
+	{
+		"%{prj.name}/dep/GLEW/lib/Release/x64",
+		"%{prj.name}/dep/GLFW/lib64-vc2019",
+		"%{prj.name}/dep/GLM/lib",
+		"%{prj.name}/dep/ImGui/lib"
+	}
+
+	links
+	{
+		"opengl32.lib",
+		"glm_static.lib",
+		"glew32s.lib",
+		"glfw3.lib",
+		"ImGui.lib",
+		"kernel32.lib",
+		"user32.lib",
+		"gdi32.lib",
+		"winspool.lib",
+		"comdlg32.lib",
+		"advapi32.lib",
+		"shell32.lib",
+		"ole32.lib",
+		"oleaut32.lib",
+		"uuid.lib",
+		"odbc32.lib",
+		"odbccp32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PLATFORM_WINDOWS",
+			"GLEW_STATIC"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Simulation")
+		}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+		buildoptions "/MDd"
+
+	filter "configurations:Release"
+		defines "RELEASE"
+		optimize "On"
+		buildoptions "/MD"
+
+project "Simulation"
+	location "Simulation"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+	
+	includedirs
+	{
+		"RenderEngine/src"
+	}
+
+	links
+	{
+		"RenderEngine.dll"
+	}
+
