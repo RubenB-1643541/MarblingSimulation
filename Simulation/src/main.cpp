3e65965aa67bb5ae802dd1d628c132fec5aa542f
ruben ballet <ruben.ballet@gmail.com>
2020-09-18 14:34:51 +0200

    Marbling Sim TODO

--- Simulation/src/main.cpp
+++ Simulation/src/main.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
 #include "Core/WindowBase.h"
-#include "Core.h"
+#include "Core/Application.h"
 #include "Core/Log.h"
-
 
 int main(int argc, char* argv[]) {
 	RenderEngine::Log::Init();
-	RenderEngine::Log::GetClientLogger()->info("Test");
-	INFO("TEST");
 	INFO("Main Started");
-	std::cout << "Hello World!" << std::endl;
-	RenderEngine::WindowBase window("Test");
-	window.Start();
-	while (true)
-		window.Update();
-	std::cout << "Done" << std::endl;
+
+	RenderEngine::Application app("Test", new RenderEngine::WindowBase("Test"));
+	app.Start();
+	
+	INFO("FINISHED");
 }
